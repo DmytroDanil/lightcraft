0f4ffa15599f5e38cac58125a984a2b5724c1f6e
DmytroDanil <97043725+DmytroDanil@users.noreply.github.com>
2022-01-09 20:46:01 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  cssMode: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// var swiper = new Swiper(".mySwiper", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
