7c8fc412a2ed6d46d22566a9c731c8e8db159e6e
DmytroDanil <97043725+DmytroDanil@users.noreply.github.com>
2022-01-16 22:40:38 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-const swiper = new Swiper('.swiper', {
-  cssMode: true,
-  autoplay: {
-    delay: 5000,
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
   },
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
   },
-});
+  Opera: function () {
+    return navigator.userAgent.match(/Opera MiniWindows/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
+};
 
-// var swiper = new Swiper(".mySwiper", {
-//   cssMode: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-//   mousewheel: true,
-//   keyboard: true,
-// });
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+  document.getElementById('menu').classList.add('_touch');
+
+  let menuClicks = document.querySelectorAll('.menu__click');
+  if (menuClicks.length > 0) {
+    for (let index = 0; index < menuClicks.length; index++) {
+      const menuClick = menuClicks[index];
+      menuClick.addEventListener("click", function (e) {
+        menuClick.parentElement.classList.toggle('_active');
+      });
+    }
+  }
+
+  let menuArrows = document.querySelectorAll('.menu__arrow');
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener("click", function (e) {
+        menuArrow.parentElement.classList.toggle('_active');
+      });
+    }
+  }
+} else {
+  document.body.classList.add('_pc');
+  document.getElementById('menu').classList.add('_pc');
+}
